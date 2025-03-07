926cc1f0a737e4ad30d4e09f78820fe434293875
MeisyNadiaNababan <meisynadianababan@gmail.com>
2025-03-08 01:05:23 +0700

    update p2_l6_jb2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
-use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -53,3 +58,7 @@ Route::get('/user/{name?}', function ($name='John') {
 
 Route::get('/hello', [WelcomeController::class,'hello']);
 
+Route::get('/hello', [WelcomeController::class,'hello']);
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'about']);
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
