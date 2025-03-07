0fbd89e0a743e46268140751b64a93a1a680636c
MeisyNadiaNababan <meisynadianababan@gmail.com>
2025-03-08 01:07:19 +0700

    update p2_l8_jb2

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\PageController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PhotoController;
 
 
 /*
